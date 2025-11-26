6c328baf61bdae0922ddec48061998f1f89f077e
kanza <kanza190504@gmail.com>
2025-11-26 19:19:30 +0700

    upgrade p4

--- coffeeshop/supplier/urls.py
+++ coffeeshop/supplier/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import SupplierListView, SupplierDetailView, SupplierCreateView
+from .views import SupplierListView, SupplierDetailView, SupplierCreateView, SupplierUpdateView, SupplierDeleteView
 
 urlpatterns = [
     path('', SupplierListView.as_view(), name='supplier-list'),
     path('tambah/', SupplierCreateView.as_view(), name='supplier-tambah'), # URL untuk form tambah
     path('<int:pk>/', SupplierDetailView.as_view(), name='supplier-detail'),
+    path('<int:pk>/edit/', SupplierUpdateView.as_view(), name='supplier-edit'),
+    path('<int:pk>/edit/', SupplierUpdateView.as_view(), name='supplier-edit'),
+    path('<int:pk>/hapus/', SupplierDeleteView.as_view(), name='supplier-hapus'), # URL untuk hapus
 
 ]
--- coffeeshop/supplier/views.py
+++ coffeeshop/supplier/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView
 from .models import Supplier
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Supplier, Customer, Barista, Order
 from .forms import SupplierForm
 
@@ -10,9 +10,20 @@ class SupplierListView(ListView):
 
 class SupplierDetailView(DetailView):
     model = Supplier
-    
+
 class SupplierCreateView(CreateView):
     model = Supplier
     form_class = SupplierForm
     template_name = 'supplier/supplier_form.html'
+    success_url = reverse_lazy('supplier-list')
+
+class SupplierUpdateView(UpdateView):
+    model = Supplier
+    form_class = SupplierForm
+    template_name = 'supplier/supplier_form.html'  
+    success_url = reverse_lazy('supplier-list')
+
+class SupplierDeleteView(DeleteView):
+    model = Supplier
+    template_name = 'supplier/supplier_confirm_delete.html'
     success_url = reverse_lazy('supplier-list')
